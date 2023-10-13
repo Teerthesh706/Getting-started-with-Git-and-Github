1b8a1b6aa44bd38dec48e68dc999a8ea8cca587c
Teerthesh Jain <teerthesh706@gmail.com>
2023-10-13 23:19:31 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,7 +2,7 @@
    # This script calculates simple interest given principal,
    # annual rate of interest and time period in years.
    # Do not use this in production. Sample purpose only.
-   # Author: Marquise Pullen
+   # Author: Teerthesh Jain
    # Additional Authors:
    # GitHub: marquise-pullen
    # Input:
