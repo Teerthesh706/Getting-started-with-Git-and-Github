680669b4b9d76aebe10539d94691abe7d3024190
Teerthesh Jain <teerthesh706@gmail.com>
2023-10-13 23:46:01 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Teerthesh Jain
    # Additional Authors:
-   # GitHub: marquise-pullen
+   # GitHub: teerthesh706
    # Input:
    # p, principal amount
    # t, time period in years
